88a297752bc7efff44a23ee54c18b1a3ea4c2bf8
JuliaKagirova <kagirova.y@inbox.ru>
2024-08-28 15:03:39 +0300

    notification home work done

--- ChuckNorrisJokes/Models/NotificationManager.swift
+++ ChuckNorrisJokes/Models/NotificationManager.swift
@@ -1,8 +1,60 @@
 //
-//  ТщешашсфешщтЬфтфпук.swift
+//  NotificationManager.swift
 //  ChuckNorrisJokes
 //
 //  Created by Юлия Кагирова on 27.08.2024.
 //
 
-import Foundation
+import UIKit
+import UserNotifications
+
+ 
+class NotificationManager {
+    
+    private var categoryID = " category1"
+    
+    init() {
+        registerCategory() 
+    }
+    
+    func requestPermission() {
+        Task {
+            try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound])
+        }
+    }
+    @MainActor
+    func checkAuthorizationStatus() async -> Bool {
+        await UNUserNotificationCenter.current().notificationSettings().authorizationStatus == .authorized
+    }
+    
+    func addNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "ChuckNorrisJokes"
+        content.body = "Посмотрите последние обновления"
+        content.badge = 1
+        content.sound = .default
+        content.userInfo = ["key": "value"]
+        content.categoryIdentifier = categoryID
+        
+        var date = DateComponents()
+        date.hour = 19
+        date.minute = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+
+        let request = UNNotificationRequest(identifier: "123", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+    func resetNotification() {
+//        UNUserNotificationCenter.current().removeAllPendingNotificationReq uests()
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["123"])
+    }
+    
+    private func registerCategory () {
+       let action = UNNotificationAction(identifier:  "actionID", title: "Push me!")
+       let category = UNNotificationCategory(identifier: categoryID, actions: [action], intentIdentifiers: [])
+        UNUserNotificationCenter.current().setNotificationCategories( [category])
+    }
+}
